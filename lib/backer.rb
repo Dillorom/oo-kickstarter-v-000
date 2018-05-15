1add3f947b0083d99542216dd5ea117279400470
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-15 19:25:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ attr_accessor :name, :backed_projects
     @backed_projects = []
   end
 
-  def back_project(Project)
+  def back_project(project)
     backer = Backer.new(name)
     project = Project.new(name)
     @backed_projects << project
