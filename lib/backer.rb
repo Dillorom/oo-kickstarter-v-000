eb03c077651c4160688b55a683b31f9f1252ee53
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-15 19:03:08 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,12 @@
 class Backer
 attr_accessor :name
-
+@backed_projects = []
   def initialize(name)
     @name = name
+  end
+
+  def back_project(project)
+    @backed_projects = [] << self
+
   end
 end
